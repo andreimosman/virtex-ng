f305c11646cc97def37d90ae27eb4142084c3c75
mosman <mosman>
2007-09-16 20:21:18 +0000

    no message

---
+++ model/PERSISTE_CBTB_ENDERECO_COBRANCA.class.php
@@ -0,0 +1,22 @@
+<?
+
+	class PERSISTE_CBTB_ENDERECO_COBRANCA extends VirtexPersiste {
+		
+		public function __construct($bd=null) {
+			parent::__construct();
+			
+			$this->_campos		= array("id_endereco_cobranca","id_cliente_produto","endereco","complemento","bairro","id_cidade","cep","id_cliente");
+			$this->_chave 		= "id_endereco_cobranca";
+			$this->_ordem		= "id_endereco_cobranca DESC";
+			$this->_tabela		= "cbtb_endereco_cobranca";
+			$this->_sequence	= "cbsq_id_endereco_cobranca";
+			
+			$this->_filtros		= array("id_endereco_cobranca" => "numeric", "id_cliente_produto" => "numeric", "id_cidade" => "numeric", "id_cliente" => "numeric");
+		
+		}
+	
+	
+	
+	}
+	
+?>
