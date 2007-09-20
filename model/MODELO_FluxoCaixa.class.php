6b3ed953ce8fe904e9b0ad38a68e1ec699815b22
mosman <mosman>
2007-09-20 02:22:12 +0000

    no message

---
+++ model/MODELO_FluxoCaixa.class.php
@@ -0,0 +1,84 @@
+<?
+
+	/**
+	 * Modelo para gerenciamento de Fluxo de Caixa.
+	 */
+	class MODELO_FluxoCaixa extends VirtexModelo {
+	
+		protected $cxtb_fluxo;
+		
+		/** 
+		 * Origem da entrada: Arquivo de Retorno 
+		 */
+		public static $TIPO_ORIGEM_RETORNO 		= 'R';
+		
+		/** 
+		 * Origem da entrada: Processamento Manual 
+		 */
+		public static $TIPO_ORIGEM_MANUAL		= 'M';
+		
+
+
+		/** 
+		 * Espécie: Dinheiro 
+		 */
+		public static $ESPECIE_DINHEIRO			= 'D';
+		
+		/** 
+		 * Espécie: Transferência ou DOC 
+		 */
+		public static $ESPECIE_TRANSFERENCIA	= 'T';
+		
+		/**
+		 * Espécie: Cheque
+		 */
+		public static $ESPECIE_CHEQUE			= 'C';
+		
+		/**
+		 * Espécie: Cartão de Crédito
+		 */
+		public static $ESPECIE_CARTAO_CREDITO	= 'R';
+
+
+		/**
+		 * Espécie: Boleto/Título 
+		 * (Usado pelo arquivo de processamento de retorno)
+		 */
+		public static $ESPECIE_BOLETO			= 'B';
+
+		
+
+		public function __construct() {
+			parent::__construct();
+			$this->cxtb_fluxo = VirtexPersiste::factory("cxtb_fluxo");
+		}
+		
+		public static function obtemListaOrigens() {
+			return(array(
+						"Arquivo de Retorno" => self::$TIPO_ORIGEM_RETORNO, 
+						"Processamento Manual" => self::$TIPO_ORIGEM_MANUAL
+						)
+					);
+		}
+		
+		public static function obtemListaEspecies() {
+			return(array(
+							"Dinheiro" => self::$ESPECIE_DINHEIRO,
+							"Cheque" => self::$ESPECIE_CHEQUE,
+							"Cartão de Crédito" => self::$ESPECIE_CARTAO_CREDITO,
+							"Transferência/DOC/Débito Automático" => self::$ESPECIE_TRANSFERENCIA,
+							"Boleto/Título" => => self::$ESPECIE_BOLETO
+						)
+					);
+		}
+		
+		
+		// public function cadastraEntrada($valor,$tipo_origem,$origem,$especie,
+	
+	
+	
+	
+	}
+
+
+?>
--- model/PERSISTE_CXTB_FLUXO.class.php
+++ model/PERSISTE_CXTB_FLUXO.class.php
@@ -10,24 +10,31 @@
 			
 			$this->_campos		= array(
 										"id_fluxo",
-										"tipo_movimentacao", 	/** (D) Débito, (C) Crédito */
+										"tipo_movimentacao", 		/** (S) Saída, (E) Entrada */
 										"valor",
-										"tipo_origem", 			/** (R) Arquivo de Retorno, (M) Entrada manual */
-										"origem", 				/** Se for tipo R indicar o nome do arquivo, 
-										                         ** se for M indicar o administrador que executou a operacao 
-										                         **/
-										"data_registro",		/** Data que ocorreu o registro do evento. */
-										"data_compensacao",		/** Data que o valor foi compensado */
-										"especie",				/** (D) Dinheiro, (T) Transferencia ou DOC, (C) Cheque */
-										"cheque_nome",			/** Nome constando no cheque */
-										"cheque_banco",			/** Código de 3 dígitos do banco */
-										"cheque_agencia",		/** Agencia (sem DV)*/
-										"cheque_conta",			/** Conta corrente (sem DV) */
-										"cheque_serie",			/** Série do cheque */
-										"cheque_numero",		/** Número do cheque */
-										"cheque_pre",			/** No caso de predatado especificar a data */
-										"id_cobranca",			/** Quando aplicável a identificação de uma cobranca em cbtb_faturas */
-										"autenticacao"			/** Sequencial que indica que o valor foi pago, usar de cxsq_autenticacao */
+										"tipo_origem", 				/** (R) Arquivo de Retorno, (M) Entrada manual */
+										"origem", 					/** Se for tipo R indicar o nome do arquivo, 
+										                        	 ** se for M indicar o administrador que executou a operacao 
+										                        	 **/
+										"data_registro",			/** Data que ocorreu o registro do evento. */
+										"data_compensacao",			/** Data que o valor foi compensado */
+										"especie",					/** (D) Dinheiro, (T) Transferencia/DOC/Débito Automático, (C) Cheque, 
+										                        	 ** (B) Boleto/Título Bancário, (R) Cartão de Crédito 
+										                        	 */
+										"cheque_nome",				/** Nome constando no cheque */
+										"cheque_banco",				/** Código de 3 dígitos do banco */
+										"cheque_agencia",			/** Agencia (sem DV)*/
+										"cheque_conta",				/** Conta corrente (sem DV) */
+										"cheque_serie",				/** Série do cheque */
+										"cheque_numero",			/** Número do cheque */
+										"cheque_pre",				/** No caso de predatado especificar a data */
+										
+										"boleto_linhadigitavel",	/** Linha digitável do Boleto/Título */
+										"boleto_codigodebarras",	/** Código de barras do Boleto/Título */
+										"transf_codigo",			/** Número de autorização do cartão de débito ou código da transferência */
+										
+										"id_cobranca",				/** Quando aplicável a identificação de uma cobranca em cbtb_faturas */
+										"autenticacao"				/** Sequencial que indica que o valor foi pago, usar de cxsq_autenticacao */
 										);
 			$this->_chave		= "id_fluxo";
 			$this->_ordem		= "data_registro DESC";
