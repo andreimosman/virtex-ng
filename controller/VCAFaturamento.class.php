fbb3834202c143145a59ff04e59be66d69228f54
dede <dede>
2007-12-24 15:05:01 +0000

    no message

--- controller/VCAFaturamento.class.php
+++ controller/VCAFaturamento.class.php
@@ -10,7 +10,7 @@ class VCAFaturamento extends VirtexControllerAdmin {
 
 	protected function init() {
 		parent::init();
-			
+
 		$this->faturamento = VirtexModelo::factory('cobranca');
 		$this->_view = VirtexViewAdmin::factory('faturamento');
 	}
@@ -22,60 +22,66 @@ class VCAFaturamento extends VirtexControllerAdmin {
 			break;
 		}
 	}
-	
-	
+
+
 	protected function executaRelatorios() {
-		
+
 		$relatorio = @$_REQUEST["relatorio"];
 
-			
+
 		if("previsao" == $relatorio){
-			
-			
+
+
 		}elseif ("faturamento" == $relatorio){
-	
-	
+
+
 		}elseif ("por_produto" == $relatorio){
-			
+
 			$ano_select = @$_REQUEST["ano_select"];
 			$this->_view->atribui("ano_select", $ano_select);
 			$this->_view->atribui("ano_select1", $ano_select1);
 			$cobranca = VirtexModelo::factory("cobranca");
 			$lista = $cobranca->obtemFaturamentoPorProduto($ano_select);
-			$dados = array();
-			$sumario = array("valor_pago"=>0);
-			
-			for($i=0;$i<count($lista);$i++) {
-					if( $lista[$i]["mes"] < 10 ) $lista[$i]["mes"] = "0".$lista[$i]["mes"];
-					$dados[ $lista[$i]["ano"] . "-" . $lista[$i]["mes"] ] = $lista[$i];
-					$tipo[ $lista[$i]["tipo"]] = $lista[$i];	
-					$sumario["valor_pago"] += $lista[$i]["valor_pago"];
+			$anos_fatura = $cobranca->obtemAnosFatura();
+			$dados_bl = array();
+			$dados_h = array();
+			$dados_d = array();
+
+			for($i=1; $i<=12; $i++) {
+				$dados_bl[$i] = "0.00";
+				$dados_h[$i] = "0.00";
+				$dados_d[$i] = "0.00";
+			}
+
+			for($i=0; $i<count($lista); $i++) {
+
+				$temp = $lista[$i];
+
+				switch($temp["tipo"]) {
+
+					case "BL":
+						$dados_bl[$temp["mes"]] = $temp["valor_pago"];
+						break;
+					case "D":
+						$dados_d[$temp["mes"]] = $temp["valor_pago"];
+						break;
+					case "H":
+						$dados_h[$temp["mes"]] = $temp["valor_pago"];
+						break;
+					default:
+						//FAZ NADa POR ENQUANTO
+						break;
 				}
-				$this->_view->atribui("sumario",$sumario);
-				$this->_view->atribui("tipo",$tipo);
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			echo "<pre>";
-			//print_r($tipo);
-			echo "</pre>";
-			echo "<pre>";
-			print_r($dados);
-			echo "</pre>";
-			
-			
-			
-	
+			}
+
+			$this->_view->atribui("ano_select",$ano_select);
+			$this->_view->atribui("anos_fatura",$anos_fatura);
+			$this->_view->atribui("dados_bl", $dados_bl);
+			$this->_view->atribui("dados_h", $dados_h);
+			$this->_view->atribui("dados_d", $dados_d);
+
 		}elseif ("por_periodo" == $relatorio){
-		
+
 			$periodo = 12;
 			$this->_view->atribui("periodo", $periodo);
 			$cobranca = VirtexModelo::factory("cobranca");
@@ -83,20 +89,20 @@ class VCAFaturamento extends VirtexControllerAdmin {
 			$lista = $cobranca->obtemFaturamentoPorPeriodo($periodo);
 			$dados = array();
 			$sumario = array("valor_documento" => 0, "valor_acrescimo" => 0, "valor_desconto" => 0, "valor_pago"=>0);
-			
+
 			for($i=0;$i<count($lista);$i++) {
 				if( $lista[$i]["mes"] < 10 ) $lista[$i]["mes"] = "0".$lista[$i]["mes"];
 				$dados[ $lista[$i]["ano"] . "-" . $lista[$i]["mes"] ] = $lista[$i];
-				
+
 				$sumario["valor_documento"] += $lista[$i]["valor_documento"];
 				$sumario["valor_desconto"] += $lista[$i]["valor_desconto"];
 				$sumario["valor_acrescimo"] += $lista[$i]["valor_acrescimo"];
 				$sumario["valor_pago"] += $lista[$i]["valor_pago"];
 			}
-			
+
 			$this->_view->atribui("sumario",$sumario);
 			$dt = date("d/m/Y");
-			
+
 			for($i=0;$i<$periodo;$i++) {
 				list($d,$m,$y) = explode("/",$dt);
 				$dt = MData::adicionaMes($dt,-1);
@@ -106,7 +112,7 @@ class VCAFaturamento extends VirtexControllerAdmin {
 			}
 			krsort($dados);
 			$this->_view->atribui("lista", $dados);
-	
+
 		}
 	}
 
