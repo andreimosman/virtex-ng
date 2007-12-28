1fc87db84f24338c3d9052308655c26460ab54f8
daniel <daniel>
2007-12-28 12:22:31 +0000

    adicionado uma chamada de count() p/ calcular a qtd de bloqueios

--- controller/VCAHome.class.php
+++ controller/VCAHome.class.php
@@ -1,6 +1,8 @@
 <?
 
 	class VCAHome extends VirtexControllerAdmin {
+		
+		protected $cobranca;
 	
 		public function __construct() {
 			parent::__construct();
@@ -9,8 +11,16 @@
 		public function init() {
 			parent::init();
 			$this->_view = VirtexViewAdmin::factory("home");
-			$this->_view->atribui("licenca",$this->licenca->obtemLicenca());
-		}	
-	
+			$this->_view->atribui("licenca",$this->licenca->obtemLicenca());	
+			$this->cobranca = VirtexModelo::factory('cobranca');
+			
+			$atrasados = $this->cobranca->obtemContratosFaturasAtrasadas();
+			$this->_view->atribui("atrasados", $atrasados);
+			
+			$countBloqueados = count($atrasados);
+			$this->_view->atribui("countBloqueados", $countBloqueados);
+		}
+		
+		
 	}
 ?>
