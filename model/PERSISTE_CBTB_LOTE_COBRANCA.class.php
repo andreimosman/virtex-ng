9e3d53a112d17809828ececae6b690b575f6f47f
dede <dede>
2007-12-28 18:22:15 +0000

    no message

--- model/PERSISTE_CBTB_LOTE_COBRANCA.class.php
+++ model/PERSISTE_CBTB_LOTE_COBRANCA.class.php
@@ -16,22 +16,38 @@ class PERSISTE_CBTB_LOTE_COBRANCA extends VirtexPersiste {
 
 
 	public function obtemUltimasRemessas($quantidade) {
-			$sql  = "SELECT id_remessa, data_geracao, periodo, data_referencia, id_admin ";
-			$sql .= "FROM cbtb_lote_cobranca ";
+
+			$sql  = "SELECT cob.id_remessa, cob.data_geracao, cob.periodo, cob.data_referencia, cob.id_admin, ";
+			$sql .= "(SELECT  count(*) FROM cbtb_lote_fatura fat WHERE fat.id_remessa = cob.id_remessa) as qtfaturas ";
+			$sql .= "FROM cbtb_lote_cobranca cob ";
 			$sql .= "ORDER BY id_remessa DESC ";
 			$sql .= "LIMIT $quantidade";
-	
+
+	echo $sql;
 			return ($this->bd->obtemRegistros($sql));
 	}
-	
+
 	public function obtemDadosLote($id_remessa) {
 			$sql  = "SELECT id_remessa, data_geracao, periodo, data_referencia, id_admin ";
 			$sql .= "FROM cbtb_lote_cobranca ";
 			$sql .= " WHERE id_remessa='$id_remessa' ";
-	
+
 			return ($this->bd->obtemRegistros($sql));
 	}
 
+
+	public function obtemListaFaturasLote($id_lote) {
+		$sql  = "SELECT ";
+		$sql .= "	fat.data, fat.valor, prd.nome, clt.nome_razao ";
+		$sql .= "FROM ";
+		$sql .= "	(cbtb_lote_fatura lfat INNER JOIN cbtb_faturas fat ON lfat.id_cobranca = fat.id_cobranca) ";
+		$sql .= "		INNER JOIN (cbtb_cliente_produto clpr INNER JOIN prtb_produto prd ON prd.id_produto = clpr.id_produto) ON fat.id_cliente_produto = clpr.id_cliente_produto ";
+		$sql .= "			INNER JOIN cltb_cliente clt ON clt.id_cliente = clpr.id_cliente ";
+		$sql .= "WHERE ";
+		$sql .= "lfat.id_remessa = $id_lote ";
+
+		return($this->bd->obtemRegistros($sql));
+	}
 
 }
 
--- view/VVACobranca.class.php
+++ view/VVACobranca.class.php
@@ -1,128 +1,140 @@
 <?
 
 	class VVACobranca extends VirtexViewAdmin {
-	
+
 		protected function __contruct() {
 			parent::__construct();
 		}
-		
+
 		protected function init() {
 			parent::init();
 			$this->nomeSessao = "Cobrança";
 		}
-		
+
 		public function exibe() {
-			
+
 			switch($this->obtem("op")) {
+
+				case 'gerar_lista_boletos':
+					$this->exibeListaBoletos();
+					break;
 
 				case 'bloqueios':
 					$this->exibeBloqueios();
 					break;
-					
+
 				case 'amortizacao':
 					$this->exibeAmortizacao();
 					break;
-					
+
 				case 'gerar_cobranca':
 					$this->exibeGerarCobranca();
 					break;
-					
+
 				case 'arquivos':
 					$this->exibeArquivos();
 					break;
-					
+
 				case 'relatorios':
 					$this->exibeRelatorios();
 					break;
-			
+
+
 			}
-		
+
 			parent::exibe();
-		
+
 		}
-		
+
+		protected function exibeListaBoletos() {
+			$this->_file = "cobranca_listar_faturas.html";
+			$this->atribui("titulo","Faturas por Lote");
+		}
+
 		protected function exibeBloqueios() {
 			$this->_file = "cobranca_bloqueios.html";
 			$this->atribui("titulo","Clientes Passíveis de Bloqueio");
 		}
-		
+
 		protected function exibeAmortizacao() {
 			$this->_file = "cobranca_amortizacao.html";
 			$this->atribui("titulo", "Amortização");
 		}
-		
+
 		protected function exibeGerarCobranca() {
+			$id_remessa = @$_REQUEST["id_remessa"];
 			$this->_file = "cobranca_gerar_cobranca.html";
+			if($id_remessa) $this->_file = "cobranca_gerar_cobranca.html";
 			$this->atribui("titulo","Gerar Cobrança/Boletos");
 		}
-		
+
 		protected function exibeArquivos() {
 			$this->_file = "cobranca_arquivos.html";
 			$this->atribui("titulo","Troca de Arquivos");
 		}
-		
+
 		protected function exibeRelatorios() {
 			$relatorio = @$_REQUEST["relatorio"];
 			$titulo = "Relatório :: ";
 			switch($relatorio) {
-				case "cortesias":					
-					$this->_file = "relatorio_cortesia.html";	
-					$this->atribui("titulo", $titulo."Cortesias");	
+				case "cortesias":
+					$this->_file = "relatorio_cortesia.html";
+					$this->atribui("titulo", $titulo."Cortesias");
 					break;
-				case "cancelamentos":					
-					$this->_file = "relatorio_cancelamento.html";	
-					$this->atribui("titulo", $titulo."Cancelamentos");	
+				case "cancelamentos":
+					$this->_file = "relatorio_cancelamento.html";
+					$this->atribui("titulo", $titulo."Cancelamentos");
 					break;
-				case "atrasos":					
-					$this->_file = "relatorio_atraso.html";	
-					$this->atribui("titulo", $titulo."Atrasos");	
+				case "atrasos":
+					$this->_file = "relatorio_atraso.html";
+					$this->atribui("titulo", $titulo."Atrasos");
 					break;
-				case "atrasos_detalhes":					
-				$this->_file = "relatorio_atraso_detalhes.html";	
-				$this->atribui("titulo", $titulo."Detalhes de Atrasos");	
+				case "atrasos_detalhes":
+				$this->_file = "relatorio_atraso_detalhes.html";
+				$this->atribui("titulo", $titulo."Detalhes de Atrasos");
 				break;
-				case "cliente_produto":					
-					$this->_file = "relatorio_cliente_produto.html";	
-					$this->atribui("titulo", $titulo."Contratos por produto");	
+				case "cliente_produto":
+					$this->_file = "relatorio_cliente_produto.html";
+					$this->atribui("titulo", $titulo."Contratos por produto");
 					break;
-				case "cliente_produto_detalhe":					
-					$this->_file = "relatorio_cliente_produto_detalhe.html";	
-					$this->atribui("titulo", $titulo."Clientes por produto");	
+				case "cliente_produto_detalhe":
+					$this->_file = "relatorio_cliente_produto_detalhe.html";
+					$this->atribui("titulo", $titulo."Clientes por produto");
 					break;
-				case "cliente_tipo_produto":					
-					$this->_file = "relatorio_cliente_tipo_produto.html";	
-					$this->atribui("titulo", $titulo."Contratos por tipo de produto");	
-					break;	
-				case "cliente_tipo_produto_detalhe":					
-					$this->_file = "relatorio_cliente_tipo_produto_detalhe.html";	
-					$this->atribui("titulo", $titulo."Clientes por tipo de produto");	
+				case "cliente_tipo_produto":
+					$this->_file = "relatorio_cliente_tipo_produto.html";
+					$this->atribui("titulo", $titulo."Contratos por tipo de produto");
+					break;
+				case "cliente_tipo_produto_detalhe":
+					$this->_file = "relatorio_cliente_tipo_produto_detalhe.html";
+					$this->atribui("titulo", $titulo."Clientes por tipo de produto");
 					break;
 				case "adesoes":
-					$this->_file = "relatorio_adesoes.html";	
+					$this->_file = "relatorio_adesoes.html";
 					$this->atribui("titulo", $titulo."Adesões");
 					break;
 				case "reagendamentos":
-					$this->_file = "relatorio_reagendamentos.html";	
+					$this->_file = "relatorio_reagendamentos.html";
 					$this->atribui("titulo", $titulo."Reagendamentos");
 					break;
 				case "bloqueios_desbloqueios":
-					$this->_file = "relatorio_bloqueios_desbloqueios.html";	
+					$this->_file = "relatorio_bloqueios_desbloqueios.html";
 					$this->atribui("titulo", $titulo."Bloqueios e Desbloqueios");
 					break;
 				case "emails_cobranca":
-					$this->_file = "relatorio_emails_cobranca.html";	
+					$this->_file = "relatorio_emails_cobranca.html";
 					$this->atribui("titulo", $titulo."Emails de Cobrança");
 					break;
 				case "evolucao":
-					$this->_file = "relatorio_evolucao.html";	
+					$this->_file = "relatorio_evolucao.html";
 					$this->atribui("titulo", $titulo."Evolução");
 					break;
-					
+
 				default:
-					die("erro");	
+					die("erro");
 			}
 		}
-		
+
 	}
-	
+
 ?>
