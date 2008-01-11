24ad6f7cc473bc37dc4623e05b948e611f2854a7
dede <dede>
2008-01-11 04:59:01 +0000

    no message

--- controller/VirtexController.class.php
+++ controller/VirtexController.class.php
@@ -6,70 +6,78 @@
 	class VirtexController extends VirtexAdmin {
 		protected $_view;			// view
 		protected $_executar;		// Indica se é pra executar a função executa();
-		
+
 		protected $_op;
 		protected $acao;
-		
+
 		protected $_script;
-		
+
 		// Recursos de autenticação
 		protected $_login;			// Objeto de autenticação
 		protected $_loginScript;	// URL p/ autenticação
 		protected $primeiroLogin;
 		protected $_changePasswordScript;
 		protected $_uri;
-		
+
 		protected $_registroScript;
-		
+
 		protected $remote_addr;
-		
+
 		public static function &factory($tipo,$controller) {
 			$tipo = strtolower($tipo);
 			$controller = strtolower($controller);
 			$retorno = $tipo == "admin" ? VirtexControllerAdmin::factory($controller) : VirtexControllerUsuario::factory($controller);
 			return $retorno;
-			
+
 		}
-		
+
 		protected function verificaLogin() {
-			
+
 			// Validação básica (comum à todas interfaces);
 			if( @$this->_login->obtemUsername() ) {
 				return(true);
 			}
-					
+
 			return(false);
-		
+
 		}
-		
+
 		protected function verificaRegistro() {
-		
+
 			if( !$this->licenca->isValid() ) {
 				return(false);
+			} else {
+				if($this->licenca->congelou()) {
+					return(false);
+				}
+
+				if($this->licenca->expirou()) {
+					$this->_view->atribui("congelamento", 1);
+				}
 			}
-			
+
 			return(true);
-		
+
 		}
-		
+
 		protected function __construct() {
 			parent::__construct();
 			// Dados padrão que podem ser sobrescritos em init()
 			$this->_executar 	= true;
-			
+
 			$this->ipaddr = $_SERVER["REMOTE_ADDR"];
-			
-			
+
+
 			$this->_login = Login::getInstance();
-			
+
 			$tmp = split("/",@$_SERVER["SCRIPT_FILENAME"]);
 			$this->_script = $tmp[ count($tmp) -1 ];
 
 			$tmp = split("/",@$_SERVER["REQUEST_URI"]);
 			$this->_uri = $tmp[ count($tmp) -1 ];
-			
+
 			unset($tmp);
-			
+
 			if( $this->_loginScript && $this->_script ) {
 				if( $this->_script != $this->_loginScript ) {
 					if( !$this->verificaLogin() ) {
@@ -81,7 +89,7 @@
 						}
 						return;
 					}
-					
+
 					if($this->primeiroLogin && $this->_changePasswordScript ) {
 						if( $this->_uri != $this->_changePasswordScript ) {
 							if( $this->_view ) {
@@ -89,12 +97,12 @@
 							} else {
 								VirtexView::simpleRedirect($this->_changePasswordScript);
 							}
-							return;						
+							return;
 						}
-						
+
 					}
-					
-				
+
+
 					if( $this->_registroScript && $this->_script ) {
 						$acao = @$_REQUEST["acao"];
 						if( $acao != "upload" && $this->_uri != $this->_registroScript ) {
@@ -110,8 +118,8 @@
 				}
 
 			}
-			
-			
+
+
 			$this->_op			= @$_REQUEST["op"];
 			$this->_acao 		= @$_REQUEST["acao"];
 
@@ -120,13 +128,13 @@
 				$this->_view->atribui("acao",$this->_acao);
 				$this->_view->atribui("dadosLogin",$this->_login->obtem("dados"));
 			}
-			
+
 			if( $this->_executar ) {
 				$this->executa();
 			}
 			$this->exibe();
 		}
-		
+
 		/**
 		 * Método para configuração do objeto
 		 *
@@ -135,23 +143,23 @@
 		protected function selfConfig() {
 			parent::selfConfig();
 		}
-		
+
 		protected function init() {
 			parent::init();
 			// Funções de Inicialização
 		}
-		
+
 		protected function executa() {
 			// Executa as functionalidades básicas
-			
+
 			// Configura a view
 		}
-		
+
 		protected function exibe() {
 			// Exibe a view
 			if( isset($this->_view) ) {
 				//print_r($this->_view);
-				$this->_view->exibe();				
+				$this->_view->exibe();
 			}
 		}
 	}
