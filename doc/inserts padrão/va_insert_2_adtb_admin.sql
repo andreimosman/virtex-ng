17f4237671fa6e232c14697338ce8a3b08ffd06d
dede <dede>
2008-02-24 06:14:15 +0000

    no message

---
+++ doc/inserts padrão/va_insert_2_adtb_admin.sql
@@ -0,0 +1,20 @@
+--
+-- PostgreSQL database dump
+--
+
+SET client_encoding = 'LATIN1';
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = public, pg_catalog;
+
+--
+-- Data for Name: adtb_admin; Type: TABLE DATA; Schema: public; Owner: virtex
+--
+
+INSERT INTO adtb_admin (id_admin, admin, senha, status, nome, email, primeiro_login, vendedor, comissionado, tipo_admin) VALUES (1, 'admin', '0192023a7bbd73250516f069df18b500', 'A', 'Administrador', 'virtex@virtex.com.br', false, NULL, NULL, NULL);
+
+--
+-- PostgreSQL database dump complete
+--
+
