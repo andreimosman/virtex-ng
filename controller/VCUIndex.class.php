63b1ed34689913e532f43971e993d8be59d66a27
amarracachorro <amarracachorro>
2008-03-05 04:08:01 +0000

    no message

--- controller/VCUIndex.class.php
+++ controller/VCUIndex.class.php
@@ -82,6 +82,7 @@
 		}
 		
 		protected function executaContratos() {
+			
 			$this->_view->atribuiVisualizacao("contratos");
 
 			$dadosLogin = $this->_login->obtem("dados");
@@ -100,10 +101,12 @@
 		}
 		
 		protected function executaContratoDetalhe() {
-			
+		
+			$dadosLogin = $this->_login->obtem("dados");
 			$this->_view->atribuiVisualizacao("contrato_detalhe");
 			
 			$id_cliente_produto = @$_REQUEST["id_cliente_produto"];
+			
 			
 			
 			//// echo $id_cliente_produto;
@@ -111,9 +114,30 @@
 			$cobranca = VirtexModelo::factory("cobranca");
 			$contrato_detalhado = $cobranca->obtemContratoPeloId($id_cliente_produto);
 			
+			$preferencias = VirtexModelo::factory("preferencias");
 			
+			$provedor = $preferencias->obtemPreferenciasProvedor();
+			
+			$provedor_geral = $preferencias->obtemPreferenciasGerais();
+			
+			$forma_pagamento = $preferencias->obtemFormaPagamento($contrato_detalhado["id_forma_pagamento"]);
+			
+			$cidade_uf = $preferencias->obtemCidadePeloID($dadosLogin["cliente"]["id_cidade"]);
+			
+			$dadosLogin["cliente"]["cidade"] = $cidade_uf["cidade"];
+			$dadosLogin["cliente"]["estado"] = $cidade_uf["uf"];
 
+			
+			$this->_view->atribui("cli",$dadosLogin["cliente"]);
 			$this->_view->atribui("contrato_detalhado",$contrato_detalhado);
+			$this->_view->atribui("nome_provedor",$provedor_geral["nome"]);
+			$this->_view->atribui("cnpj_provedor",$provedor["cnpj"]);
+			$this->_view->atribui("produto",$contrato_detalhado["nome_produto"]);
+			$this->_view->atribui("tipo_produto",$contrato_detalhado["tipo_produto"]);
+			$this->_view->atribui("localidade",$provedor["localidade"]);
+			$this->_view->atribui("valor_contrato",$contrato_detalhado["valor_produto"]);
+			$this->_view->atribui("download_kbps", $contrato_detalhado["bl_banda_download_kbps"]);
+			$this->_view->atribui("forma_pagamento",$forma_pagamento["descricao"]);
 
 			echo "<pre>";
 			print_r($contrato_detalhado);
