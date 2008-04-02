59e2c7e4129bcb393cba93cba5095ed0fc1d853f
dede <dede>
2008-04-02 15:06:22 +0000

    Mudando o campo macaddr para mac, tirando o campo usa_mac_ap

--- model/PERSISTE_CFTB_POP.class.php
+++ model/PERSISTE_CFTB_POP.class.php
@@ -6,14 +6,14 @@
 			parent::__construct();
 			
 			$this->_campos		= array("id_pop","nome","info","tipo","id_pop_ap","status","ipaddr","snmp_rw_com","infoserver",
-										"snmp_versao", "snmp_ro_com", "ativar_snmp", "ativar_monitoramento","id_servidor", "macaddr", "bloquearmac", "clientemacpop", "mac", "usa_mac_ap");
+										"snmp_versao", "snmp_ro_com", "ativar_snmp", "ativar_monitoramento","id_servidor", "bloquearmac", "clientemacpop", "mac");
 			$this->_chave 		= "id_pop";
 			$this->_tabela 		= "cftb_pop";
 			$this->_sequence 	= "cfsq_id_pop";
 			$this->_ordem		= "nome";
 			
 			$this->_filtros		= array("id_pop_ap" => "custom", "status" => "custom", "ativar_snmp" => "bool",
-										"ativar_monitoramento" => "bool", "bloqueramac" => "bool", "clientemacpop" => "bool", "usa_mac_ap" => "bool");
+										"ativar_monitoramento" => "bool", "bloqueramac" => "bool", "clientemacpop" => "bool");
 			
 		}
 		
