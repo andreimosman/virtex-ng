c02bf9a50af373c93b5207606d43bf08a9252f1a
mosman <mosman>
2008-04-03 15:37:26 +0000

    no message

---
+++ doc/altereracao_id_cliente_para_int4.sql
@@ -0,0 +1,8 @@
+
+ALTER TABLE cltb_cliente ALTER COLUMN id_cliente TYPE int;
+ALTER TABLE dominio ALTER COLUMN id_cliente TYPE int;
+ALTER TABLE cbtb_cliente_produto ALTER COLUMN id_cliente TYPE int;
+ALTER TABLE cntb_conta ALTER COLUMN id_cliente TYPE int;
+ALTER TABLE hdtb_chamado ALTER COLUMN id_cliente TYPE int;
+ALTER TABLE cntb_endereco_instalacao ALTER COLUMN id_cliente TYPE int;
+ALTER TABLE lgtb_emails_cobranca ALTER COLUMN id_cliente TYPE int;
