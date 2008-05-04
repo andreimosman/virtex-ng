3093286d3118ca1cdfd8d15afdaf66d8597f21be
mosman <mosman>
2008-05-04 19:46:23 +0000

    no message

---
+++ view/VVASetup.class.php
@@ -0,0 +1,18 @@
+<?
+
+	class VVASetup extends VirtexViewAdmin {
+
+		public function __construct() {
+			parent::__construct();
+			
+		}
+
+		public function exibe() {
+			$this->_file = "setup.html";
+			parent::exibe();
+		}
+
+
+	}
+
+?>
